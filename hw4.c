4913d323dbd429bdaa8fc5e967677d6c058ed19d
E1B41 <u11202241@o365.ttu.edu.tw>
2024-05-31 15:14:25 +0800

    第二次修改，已經完成a部分。光a部分我就覺得有點難了，因為我輸入錯誤時，一開始沒有出現輸入錯誤重新輸入，我是後面一直修改才讓他跑出來。

--- hw4.c
+++ hw4.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
-int main(void) {     /*個人風格*/ 
+#define MAX_NAME_LENGTH 50
+
+typedef struct {
+    char name[MAX_NAME_LENGTH];
+    int student_id;
+    int math;
+    int physics;
+    int english;
+} Student;
+
+int main(void) {
     puts("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
     puts("|                                                                                       |");
     puts("|  ++++++++++++++       ---        $$$$$$$$$$$$$                %%%            ***      |");
@@ -31,8 +42,9 @@ int main(void) {     /*個人風格*/
     system("cls");
 
     int password, i;
-    for (i = 3; i > 0; i--) {   /*使用者輸入密碼，最多三次*/ 
-        printf("請輸入密碼:");  /*輸入密碼*/ 
+    char choice;
+    for (i = 3; i > 0; i--) {
+        printf("請輸入密碼:");
         scanf("%d", &password);
         if (password == 2024) {
             printf("密碼正確。\n");
@@ -46,18 +58,63 @@ int main(void) {     /*個人風格*/
             puts("| d. Grade ranking               |");
             puts("| e. Exit system                 |");
             puts("----------------------------------");
-            
-            
-        } else {   /*判斷是否是2024*/ 
-            printf("剩餘%d次機會\n", i-1);
-            if (i == 1) {   /*輸入最後一次且錯誤時，發出警告音*/ 
+
+            while (1) {
+                printf("請輸入選項：");
+                choice = getch();
+                printf("%c\n", choice);
+                if (choice == 'a') {
+                    int n;
+                    printf("請輸入學生數目 (5~10): ");
+                    while (scanf("%d", &n) != 1 || n < 5 || n > 10) {
+                        printf("輸入錯誤，請輸入5到10之間的整數: ");
+                        while (getchar() != '\n'); // 清除輸入緩衝區
+                    }
+
+                    Student students[n];
+                    for (int j = 0; j < n; j++) {
+                        printf("輸入學生 %d 資料:\n", j + 1);
+                        printf("姓名: ");
+                        scanf("%s", students[j].name);
+                        printf("學號 (6位整數): ");
+                        while (scanf("%d", &students[j].student_id) != 1 || students[j].student_id < 100000 || students[j].student_id > 999999) {
+                            printf("輸入錯誤，請輸入6位整數的學號: ");
+                            while (getchar() != '\n'); // 清除輸入緩衝區
+                        }
+                        printf("數學成績 (0~100): ");
+                        while (scanf("%d", &students[j].math) != 1 || students[j].math < 0 || students[j].math > 100) {
+                            printf("輸入錯誤，請輸入0到100之間的成績: ");
+                            while (getchar() != '\n'); // 清除輸入緩衝區
+                        }
+                        printf("物理成績 (0~100): ");
+                        while (scanf("%d", &students[j].physics) != 1 || students[j].physics < 0 || students[j].physics > 100) {
+                            printf("輸入錯誤，請輸入0到100之間的成績: ");
+                            while (getchar() != '\n'); // 清除輸入緩衝區
+                        }
+                        printf("英文成績 (0~100): ");
+                        while (scanf("%d", &students[j].english) != 1 || students[j].english < 0 || students[j].english > 100) {
+                            printf("輸入錯誤，請輸入0到100之間的成績: ");
+                            while (getchar() != '\n'); // 清除輸入緩衝區
+                        }
+                    }
+                    printf("已成功輸入學生資料。\n");
+                } 
+                else {
+                    printf("無效選項。\n");
+                }
+            }
+            break;
+        } else {
+            printf("剩餘%d次機會\n", i - 1);
+            if (i == 1) {
                 printf("%c", '\a');
                 return 0;
             }
         }
     }
+
+    system("pause");
+    system("cls");
+    return 0;
 }
 
-		
-    
-    
